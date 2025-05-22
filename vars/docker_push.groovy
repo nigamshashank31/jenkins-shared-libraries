a63919aaf0c9455852a2694253cc8578b3b06a6c
nigamshashank31 <133131747+nigamshashank31@users.noreply.github.com>
2025-05-22 21:17:11 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,9 @@
 def call(String Project, String ImageTag, String dockerhubuser){
+  echo 'Pushing Docker image to Docker Hub'
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
       sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
   }
+  sh """docker image tag django_app $dockerHubUser/django_app"""
   sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
 }
 
